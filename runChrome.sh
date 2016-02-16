d7101b9b9e5d3e36399dce4c640206d09f362ac6
damianodistefano <damiano.di.stefano@gmail.com>
2016-02-16 10:43:48 +0100

    last last commit

--- runChrome.sh
+++ runChrome.sh
@@ -6,3 +6,5 @@ jflex/jflex-1.6.1/bin/jflex -d . jflex/minic.jflex
 java -jar cup/java-cup-11b.jar -locations -interface -parser Parser -xmlactions cup/minic.cup
 javac -cp cup/java-cup-11b-runtime.jar:. *.java
 java -cp cup/java-cup-11b-runtime.jar:. Parser input.c output/simple.xml /
+CURDIR="$(pwd)"
+open /Applications/Google\ Chrome.app --args --allow-file-access-from-files ${CURDIR}'/output/svg.html'
